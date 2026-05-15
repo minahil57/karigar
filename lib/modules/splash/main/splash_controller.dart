0b4e71f8a37f54e0094eae38c2e34c8077eb946c
minahil57 <minahil.akhtar10@gmail.com>
2026-05-16 02:21:55 +0500

    Added splash conditions

--- lib/modules/splash/main/splash_controller.dart
+++ lib/modules/splash/main/splash_controller.dart
@@ -3,12 +3,19 @@ import 'dart:async';
 import 'package:karigar/export.dart';
 
 class SplashController extends GetxController {
-
   @override
   void onInit() {
     super.onInit();
-    Timer(const Duration(seconds: 3), () {
-      Get.offAllNamed(Routes.onboarding);
+    Timer(const Duration(seconds: 3), () async {
+      if (await getAccessToken() == '') {
+        Get.offAllNamed(Routes.login);
+      } else {
+        if (getUser()?.role == UserRole.customer.apiValue) {
+          Get.offAllNamed(Routes.agent);
+        } else {
+          Snackbars.error("Invalid user role");
+        }
+      }
     });
   }
 }
